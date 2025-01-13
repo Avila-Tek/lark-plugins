e6b7c2508891f827c9edd5e6a53248babd7698fd
Luis <lespinerua@gmail.com>
2025-01-13 10:07:55 -0400

    feat: notes

--- email-deploy/entrypoint.sh
+++ email-deploy/entrypoint.sh
@@ -1,10 +1,17 @@
 now=$(date +'%Y-%m-%d %H:%M:%S')
+notes=$(gh release view -R $REPOSITORY --json body -q .body)
+plain_text_notes=$(echo $notes) 
+echo ""
+echo $plain_text_notes 
+echo ""
 data='{
   "app_name": "'$APP_NAME'",
   "version": "'$VERSION'",
   "date": "'$now'",
   "links": [
     { "url": "'$LINK'" }
-  ]
+  ],
+  "notes": "'$plain_text_notes'"
 }'
+echo $data
 postmark email template -f=noreply@avilatek.com -t=$RECEIVE_EMAIL -a=deploy-new -m="$data"
